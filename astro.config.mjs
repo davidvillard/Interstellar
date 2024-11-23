c6e53ac2c6bd26295be05503fd11d9e1d9646745
p4zd <davidvillard6@gmail.com>
2024-11-23 13:32:56 +0100

    proximamente: añadir backend de registro con next-auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
-  adapter: node({
-		mode: "standalone"
-	}) 
+  integrations: [tailwind()]
 });
